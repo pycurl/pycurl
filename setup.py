8233f9ab2867353fa91bd4fb30ffee5d54538dc6
Kjetil Jacobsen <kjetilja@gmail.com>
2001-06-04 19:30:04 +0000

    added setup script to generate distributions

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name="pycurl",
+      version="0.1",
+      description="PycURL",
+      author="Kjetil Jacobsen",
+      author_email="kjetilja@cs.uit.no",
+      url="http://pycurl.sourceforge.net/",
+      ext_modules=[Extension("curl", ["src/curl.c"],
+                             include_dirs=["include"],
+                             library_dirs=["lib"],
+                             libraries=["curl"]),]
+      )
