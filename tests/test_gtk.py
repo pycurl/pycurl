ad8016f4fbf7848b20a33d93833169824f85269d
Kjetil Jacobsen <kjetilja@gmail.com>
2001-08-21 10:08:22 +0000

    - progress bar did not have the necessary wrappings to make it work for threading.  should be much better now!

--- tests/test_gtk.py
+++ tests/test_gtk.py
@@ -12,21 +12,21 @@ import pycurl
 
 def progress(download_t, download_d, upload_t, upload_d):
     global round, pbar
+    threads_enter()
     if download_t == 0:
         pbar.set_activity_mode(1)
         round = round + 0.1
-        if round >= 1.0:
-            round = 0.0
+        if round >= 1.0:  round = 0.0
     else:
         pbar.set_activity_mode(0)
         round = float(download_d) / float(download_t)
     pbar.update(round)
+    threads_leave()
     return 0 # Anything else indicates an error
 
 
 def close_app(*args):
     global t, win
-    t.join()
     win.destroy()
     mainquit()
     return TRUE
@@ -34,10 +34,12 @@ def close_app(*args):
 
 class Test(threading.Thread):
 
-    def __init__(self, url):
+    def __init__(self, url, target_file):
         threading.Thread.__init__(self)
+        self.target_file = target_file
         self.curl = pycurl.init()
         self.curl.setopt(pycurl.URL, url)
+        self.curl.setopt(pycurl.FILE, target_file)
         self.curl.setopt(pycurl.FOLLOWLOCATION, 1)
         self.curl.setopt(pycurl.NOPROGRESS, 0)
         self.curl.setopt(pycurl.PROGRESSFUNCTION, progress)
@@ -46,10 +48,12 @@ class Test(threading.Thread):
     def run(self):
         self.curl.perform()
         self.curl.cleanup()        
+        self.target_file.close()
+        
 
 # Check command line args
-if len(sys.argv) < 2:
-    print "Usage: %s <URI>" % sys.argv[0]
+if len(sys.argv) < 3:
+    print "Usage: %s <URL> <filename>" % sys.argv[0]
     raise SystemExit
 
 # Launch a window with a statusbar
@@ -72,8 +76,9 @@ win.connect("destroy", close_app)
 win.connect("delete_event", close_app)
 
 # Start thread for fetching url
+f = open(sys.argv[2], 'w')
 round = 0.0
-t = Test(sys.argv[1])
+t = Test(sys.argv[1], f)
 t.start()
 
 # Start GTK mainloop
