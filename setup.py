dde6c371eb835f660d2a4bbbe4eb2a6a14134a36
Kjetil Jacobsen <kjetilja@gmail.com>
2001-11-28 09:55:32 +0000

    - some fixes to the install stuff

--- setup.py
+++ setup.py
@@ -1,16 +1,53 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
-# $Id$
+"""Setup script for the pyCURL module distribution."""
 
-from distutils.core import setup, Extension
+import os, sys
+from distutils.core import setup
+from distutils.extension import Extension
+from string import strip, split
 
-setup(name="pycurl",
-      version="0.4.3",
-      description="PycURL -- cURL library module for Python",
-      author="Kjetil Jacobsen",
-      author_email="kjetilja@cs.uit.no",
-      url="http://pycurl.sourceforge.net/",
-      ext_modules=[Extension("pycurl", ["src/curl.c"],
-                             include_dirs=["include"],
-                             libraries=["curl"]),]
-      )
+if sys.platform == "win32":
+    # Windows users have to tweak the locations on some of the paths here
+    # to match their libcurl install
+    include_dirs = [r'C:\User\clib\libcurl\include']
+    library_dirs = [r'C:\User\clib\libcurl\lib']
+    libraries = ['libcurl', 'zlib', 'msvcrt', 'libcmt', 'wsock32', 'advapi32']
+    runtime_library_dirs = []
+    extra_link_args = ['/NODEFAULTLIB:LIBCMTD.lib']
+    extra_objects = [r'C:\User\clib\libcurl\lib\libcurl.lib']
+else:
+    # Otherwise, be brave and try to figure out dynamically through curl-config
+    include_dirs = []
+    cflags=split(strip(os.popen('curl-config --cflags').read()), ' ')
+    for e in cflags:
+        if e[:2] == '-I':
+            include_dirs.append(e[2:])
+    library_dirs = []
+    libs = split(strip(os.popen('curl-config --libs').read()), ' ')
+    for e in libs:
+        if e[:2] == '-L':
+            library_dirs.append(e[2:])
+            libs.remove(e)
+    libraries = ["curl"]
+    runtime_library_dirs = []
+    extra_link_args = libs 
+    extra_objects = []
+
+long_description = "PycURL -- cURL library module for Python"
+
+setup (	name="pycurl",
+      	version="0.4.3",
+      	description="PycURL -- cURL library module for Python",
+      	author="Kjetil Jacobsen",
+      	author_email="kjetilja@cs.uit.no",
+      	url="http://pycurl.sourceforge.net/",
+      	ext_modules=[Extension(name="pycurl", 
+                               sources=["src/curl.c"],
+                               include_dirs=include_dirs,
+                               library_dirs=library_dirs,
+                               runtime_library_dirs=runtime_library_dirs,
+                               libraries=libraries,
+                               extra_link_args=extra_link_args,
+                               extra_objects=extra_objects)]
+        )	
