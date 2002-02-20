ed30fb7904693b026c4eba679e68558e82303181
Kjetil Jacobsen <kjetilja@gmail.com>
2002-02-20 15:31:49 +0000

    updated to use the xmlrpc lib in python2.2 for marshalling

--- tests/test_xmlrpc.py
+++ tests/test_xmlrpc.py
@@ -3,32 +3,27 @@
 ## PycURL module
 import pycurl
 
+## XML-RPC lib included in python2.2
+import xmlrpclib
+
 # Header fields passed in request
 xmlrpc_header = [
     "User-Agent: PycURL XML-RPC Test", "Content-Type: text/xml"
     ]
 
-# XML-RPC request body
-xmlrpc_req = """
-<?xml version='1.0'?>
-<methodCall>
-<methodName>examples.getStateName</methodName>
-<params>
-<param>
-<value><int>5</int></value>
-</param>
-</params>
-</methodCall>
+# XML-RPC request template
+xmlrpc_template = """
+<?xml version='1.0'?><methodCall><methodName>%s</methodName>%s</methodCall>
 """
 
 # Engage
 c = pycurl.init()
 c.setopt(pycurl.URL, 'http://betty.userland.com/RPC2')
-c.setopt(pycurl.FOLLOWLOCATION, 1)
-c.setopt(pycurl.MAXREDIRS, 5)
 c.setopt(pycurl.POST, 1)
 c.setopt(pycurl.HTTPHEADER, xmlrpc_header)
-c.setopt(pycurl.POSTFIELDS, xmlrpc_req)
+req = xmlrpc_template % ("examples.getStateName", xmlrpclib.dumps((5,)))
+c.setopt(pycurl.POSTFIELDS, req)
+
 print 'Response from http://betty.userland.com/'
 c.perform()
 c.cleanup()
