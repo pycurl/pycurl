443476226bc73752cc82790746c92b9925abbc92
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-04-11 15:18:31 +0000

    Added the tests/test_post.py example.

---
+++ tests/test_post.py
@@ -0,0 +1,19 @@
+# $Id$
+
+import pycurl
+
+# note: multiple fields are separated by a single '&'
+#   FIXME: how can you include a verbatim '&' in a value ??
+
+# simple
+pf = 'field1=value1'
+
+# multiple fields
+pf = 'field1=value1&field2=value2 with blanks&field3=value3'
+
+c = pycurl.init()
+c.setopt(pycurl.URL, 'http://pycurl.sourceforge.net/testpostvars.php')
+c.setopt(pycurl.POST, 1)
+c.setopt(pycurl.POSTFIELDS, pf)
+c.perform()
+c.cleanup()
