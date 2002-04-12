4a262d3511066453c7e070ec7f8e715a051d8184
Kjetil Jacobsen <kjetilja@gmail.com>
2002-04-12 13:50:47 +0000

    http posting using httppost instead of using postfields

---
+++ tests/test_post2.py
@@ -0,0 +1,13 @@
+# $Id$
+
+import urllib
+import pycurl
+
+pf = ['field1=this is a test using httppost & stuff', 'field2=value2']
+
+c = pycurl.init()
+c.setopt(pycurl.URL, 'http://pycurl.sourceforge.net/tests/testpostvars.php')
+c.setopt(pycurl.POST, 1)
+c.setopt(pycurl.HTTPPOST, pf)
+c.perform()
+c.cleanup()
