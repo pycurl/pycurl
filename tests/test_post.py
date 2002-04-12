6fdf6df91d359f2d973c15996f31bb51055e90e0
Kjetil Jacobsen <kjetilja@gmail.com>
2002-04-12 09:24:17 +0000

    make all the field examples work

--- tests/test_post.py
+++ tests/test_post.py
@@ -4,10 +4,10 @@ import urllib
 import pycurl
 
 # simple
-pf = 'field1=value1'
+pf = {'field1': 'value1'}
 
 # multiple fields
-pf = 'field1=value1&field2=value2 with blanks&field3=value3'
+pf = {'field1':'value1', 'field2':'value2 with blanks', 'field3':'value3'}
 
 # multiple fields with & in field
 pf = {'field1':'value1', 'field2':'value2 with blanks and & chars',
