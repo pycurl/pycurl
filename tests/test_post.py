b08d71a8e011dd3eb2aa1d3ccdc111ee04a7af69
Kjetil Jacobsen <kjetilja@gmail.com>
2002-04-12 09:22:46 +0000

    fixed the fixme for fields

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,9 +1,7 @@
 # $Id$
 
+import urllib
 import pycurl
-
-# note: multiple fields are separated by a single '&'
-#   FIXME: how can you include a verbatim '&' in a value ??
 
 # simple
 pf = 'field1=value1'
@@ -11,9 +9,13 @@ pf = 'field1=value1'
 # multiple fields
 pf = 'field1=value1&field2=value2 with blanks&field3=value3'
 
+# multiple fields with & in field
+pf = {'field1':'value1', 'field2':'value2 with blanks and & chars',
+      'field3':'value3'}
+
 c = pycurl.init()
 c.setopt(pycurl.URL, 'http://pycurl.sourceforge.net/tests/testpostvars.php')
 c.setopt(pycurl.POST, 1)
-c.setopt(pycurl.POSTFIELDS, pf)
+c.setopt(pycurl.POSTFIELDS, urllib.urlencode(pf))
 c.perform()
 c.cleanup()
