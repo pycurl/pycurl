6e62d6d6c1b0ef2e197b32b83064a2cc040105db
Kjetil Jacobsen <kjetilja@gmail.com>
2002-06-25 08:20:23 +0000

    bump version numbers for cvs head

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.platform[:-1] == "darwin":
 ###############################################################################
 
 setup (name="pycurl",
-       version="7.9.8",
+       version="7.9.9",
        description="PycURL -- cURL library module for Python",
        author="Kjetil Jacobsen",
        author_email="kjetilja@cs.uit.no",
