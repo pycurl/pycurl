edf2efa9827e4d27f202ff9a967760d0dc3c1d77
Kjetil Jacobsen <kjetilja@gmail.com>
2002-07-02 14:29:15 +0000

    do more than one transfer at a time, one is pretty boring for a multi interface

--- tests/test_multi.py
+++ tests/test_multi.py
@@ -2,13 +2,18 @@
 
 import pycurl
 m = pycurl.multi_init()
-c = pycurl.init()
-c.setopt(pycurl.URL, 'http://curl.haxx.se')
-m.add_handle(c)
+c1 = pycurl.init()
+c2 = pycurl.init()
+c1.setopt(pycurl.URL, 'http://curl.haxx.se')
+c2.setopt(pycurl.URL, 'http://python.org')
+m.add_handle(c1)
+m.add_handle(c2)
 while 1:
     num_handles = m.perform()
     if num_handles == 0:
         break
-m.remove_handle(c)
+m.remove_handle(c2)
+m.remove_handle(c1)
 m.cleanup()
-c.cleanup()
+c1.cleanup()
+c2.cleanup()
