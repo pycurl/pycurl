dbea2261fbe92f2b91bfdee60c48503ac56984b1
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-07-04 22:10:43 +0000

    New test scripts to check the new error handling.

---
+++ tests/test_internals.py
@@ -0,0 +1,102 @@
+# $Id$
+# vi:ts=4:et
+
+import pycurl
+print "Testing", pycurl.version
+print pycurl.__file__
+
+
+#####
+##### self-test assertion section
+#####
+
+
+# remove an invalid handle: this should fail
+if 1:
+    m = pycurl.multi_init()
+    c = pycurl.init()
+    try:
+        m.remove_handle(c)
+    except pycurl.error:
+        pass
+    else:
+        assert 0, "internal error"
+    del m, c
+
+
+# remove an invalid but closed handle
+if 1:
+    m = pycurl.multi_init()
+    c = pycurl.init()
+    c.cleanup()
+    m.remove_handle(c)
+    del m, c
+
+
+# add a closed handle: this should fail
+if 1:
+    m = pycurl.multi_init()
+    c = pycurl.init()
+    c.cleanup()
+    try:
+        m.add_handle(c)
+    except pycurl.error:
+        pass
+    else:
+        assert 0, "internal error"
+    m.cleanup()
+    del m, c
+
+
+# add a handle twice: this should fail
+if 1:
+    m = pycurl.multi_init()
+    c = pycurl.init()
+    m.add_handle(c)
+    try:
+        m.add_handle(c)
+    except pycurl.error:
+        pass
+    else:
+        assert 0, "internal error"
+    del m, c
+
+
+# add a handle on multiple stacks: this should fail
+if 1:
+    m1 = pycurl.multi_init()
+    m2 = pycurl.multi_init()
+    c = pycurl.init()
+    m1.add_handle(c)
+    try:
+        m2.add_handle(c)
+    except pycurl.error:
+        pass
+    else:
+        assert 0, "internal error"
+    del m1, m2, c
+
+
+# move a handle
+if 1:
+    m1 = pycurl.multi_init()
+    m2 = pycurl.multi_init()
+    c = pycurl.init()
+    m1.add_handle(c)
+    m1.remove_handle(c)
+    m2.add_handle(c)
+    del m1, m2, c
+
+
+# basic check of reference counting
+if 1:
+    c = pycurl.init()
+    m = pycurl.multi_init()
+    m.add_handle(c)
+    del m
+    m = pycurl.multi_init()
+    c.cleanup()
+    del m, c
+
+
+print "All tests passed."
---
+++ tests/test_multi3.py
@@ -0,0 +1,80 @@
+# $Id$
+# vi:ts=4:et
+
+# same as test_multi2.py, but enforce some debugging and strange API-calls
+
+import os, sys, time
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+import pycurl
+
+
+urls = (
+    "http://curl.haxx.se",
+    "http://www.python.org",
+    "http://pycurl.sourceforge.net",
+    "http://pycurl.sourceforge.net/THIS_HANDLE_IS_CLOSED",
+)
+
+
+# init
+m = pycurl.multi_init()
+m.handles = []
+for url in urls:
+    c = pycurl.init()
+    # save info in standard Python attributes
+    c.url = url
+    c.body = StringIO()
+    c.debug = 0
+    m.handles.append(c)
+    # pycurl API calls
+    c.setopt(pycurl.URL, c.url)
+    c.setopt(pycurl.WRITEFUNCTION, c.body.write)
+    m.add_handle(c)
+
+# debug - close a handle
+if 1:
+    c = m.handles[3]
+    c.debug = 1
+    c.cleanup()
+
+# get data
+while 1:
+    num_handles = m.perform()
+    if num_handles == 0:
+        break
+    # currently no more I/O is pending, could do something in the meantime
+    # (display a progress bar, etc.)
+    time.sleep(0.1)
+
+# close handles
+for c in m.handles:
+    # save info in standard Python attributes
+    try:
+        c.http_code = c.getinfo(pycurl.HTTP_CODE)
+    except pycurl.error:
+        # handle already closed - see debug above
+        assert c.debug
+        c.http_code = -1
+    # pycurl API calls
+    if 0:
+        m.remove_handle(c)
+        c.cleanup()
+    else:
+        # in the C API this is the wrong calling order, but pycurl
+        # handles this automatically
+        c.cleanup()
+        m.remove_handle(c)
+m.cleanup()
+
+# print result
+for c in m.handles:
+    data = c.body.getvalue()
+    if 0:
+        print "**********", c.url, "**********"
+        print data
+    else:
+        print "%-53s http_code %3d, %6d bytes" % (c.url, c.http_code, len(data))
+
