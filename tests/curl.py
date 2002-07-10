2caa888116f448dd5609b16fd292744d9171d617
Kjetil Jacobsen <kjetilja@gmail.com>
2002-07-10 11:33:43 +0000

    add more info (getinfo stuff) when accessing the info() method of the curl class

--- tests/curl.py
+++ tests/curl.py
@@ -43,13 +43,29 @@ class Curl:
         if self.h != []:
             self.c.setopt(pycurl.HTTPHEADER, self.h)
         self.c.perform()
-        self.status = self.c.getinfo(pycurl.HTTP_CODE)
-        return self.status
+        return self.c.getinfo(pycurl.HTTP_CODE)
 
     def info(self):
         self.server_reply.seek(0,0)
-        self.server_reply.readline() # discard http return code
-        return mimetools.Message(self.server_reply)
+        self.server_reply.readline() # FIXME: won't work well on non-http replies
+        m = mimetools.Message(self.server_reply)
+        m['http-code'] = str(self.c.getinfo(pycurl.HTTP_CODE))
+        m['total-time'] = str(self.c.getinfo(pycurl.TOTAL_TIME))
+        m['namelookup-time'] = str(self.c.getinfo(pycurl.NAMELOOKUP_TIME))
+        m['connect-time'] = str(self.c.getinfo(pycurl.CONNECT_TIME))
+        m['pretransfer-time'] = str(self.c.getinfo(pycurl.PRETRANSFER_TIME))
+        m['redirect-time'] = str(self.c.getinfo(pycurl.REDIRECT_TIME))
+        m['redirect-count'] = str(self.c.getinfo(pycurl.REDIRECT_COUNT))
+        m['size-upload'] = str(self.c.getinfo(pycurl.SIZE_UPLOAD))
+        m['size-download'] = str(self.c.getinfo(pycurl.SIZE_DOWNLOAD))
+        m['speed-upload'] = str(self.c.getinfo(pycurl.SPEED_UPLOAD))
+        m['header-size'] = str(self.c.getinfo(pycurl.HEADER_SIZE))
+        m['request-size'] = str(self.c.getinfo(pycurl.REQUEST_SIZE))
+        m['content-length-download'] = str(self.c.getinfo(pycurl.CONTENT_LENGTH_DOWNLOAD))
+        m['content-length-upload'] = str(self.c.getinfo(pycurl.CONTENT_LENGTH_UPLOAD))
+        m['effective-url'] = self.c.getinfo(pycurl.EFFECTIVE_URL)
+        m['content-type'] = self.c.getinfo(pycurl.CONTENT_TYPE)
+        return m
 
     def close(self):
         self.c.cleanup()
@@ -63,4 +79,5 @@ class Curl:
 if __name__ == "__main__":
     c = Curl('http://curl.haxx.se/')
     c.retrieve()
+    print c.info()
     c.close()
