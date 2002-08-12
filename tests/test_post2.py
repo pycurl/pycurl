ccb5309dcfc006cb9cfbed5939bcf67d43e15af3
Kjetil Jacobsen <kjetilja@gmail.com>
2002-08-12 13:46:25 +0000

    just a minor update

--- tests/test_post2.py
+++ tests/test_post2.py
@@ -2,6 +2,7 @@
 
 import pycurl
 
+
 pf = ['field1=this is a test using httppost & stuff', 'field2=value2']
 
 print "\n** INFO: For some reason this script does not work any longer, use the"
@@ -9,7 +10,6 @@ print "** scheme in test_post.py for posting instead!\n\n"
 
 c = pycurl.Curl()
 c.setopt(c.URL, 'http://pycurl.sourceforge.net/tests/testpostvars.php')
-c.setopt(c.POST, 1)
 c.setopt(c.HTTPPOST, pf)
 c.setopt(c.VERBOSE, 1)
 c.perform()
