6ae1a99c888f6ec9747e8ab0a0e30dfb0d67e8fb
Kjetil Jacobsen <kjetilja@gmail.com>
2002-08-12 13:31:31 +0000

    print banner with information about this script not working any longer

--- tests/test_post2.py
+++ tests/test_post2.py
@@ -4,6 +4,9 @@ import pycurl
 
 pf = ['field1=this is a test using httppost & stuff', 'field2=value2']
 
+print "\n** INFO: For some reason this script does not work any longer, use the"
+print "** scheme in test_post.py for posting instead!\n\n"
+
 c = pycurl.Curl()
 c.setopt(c.URL, 'http://pycurl.sourceforge.net/tests/testpostvars.php')
 c.setopt(c.POST, 1)
