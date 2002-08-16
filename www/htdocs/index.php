952f86cb595420a51d30c0926123f5a13f10ab58
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-08-16 15:53:57 +0000

    Updated.

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-               
+
   <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
   <title>PycURL</title>
-                           
+
   <meta name="author" content="Kjetil Jacobsen">
-               
+
   <meta name="description" content="Homepage">
 </head>
 <body bgcolor="#ffffff" link="#0000ee" vlink="#551a8b" alink="#0000ee">
@@ -27,13 +27,29 @@ For Win32 users there's also an
 <a href="download/pycurl-7.9.8.3.win32-py2.2.exe">installer with a precompiled module</a>.
 You still should download the sources because of the test and example programs.
 <br>
-<br>
-    &gt;&gt;&gt;
-<p></p>
-<a href="http://curl.haxx.se"><img border="0"
-src="http://curl.haxx.se/pix/powered_by_curl.gif"></a>
-<a href="http://sourceforge.net"><img
-src="http://sourceforge.net/sflogo.php?group_id=28236&type=1" width="88" height="31" border="0" alt="SourceForge.net Logo"></a>
+
+    <br />
+    <br />
+    <hr />
+    <div align="right">
+      <table align="right">
+        <tr align="right">
+          <td><a href="http://curl.haxx.se/"><img
+	  src="http://curl.haxx.se/pix/powered_by_curl.gif" alt=""
+          width="88" height="31" border="0" /></a>
+          </td>
+
+          <td><a href="http://sourceforge.net/"><img
+          src="http://sourceforge.net/sflogo.php?group_id=28236&amp;type=1"
+          width="88" height="31" border="0" alt="" /></a>
+          </td>
+        </tr>
+      </table>
+    </div>
+
+    <font size="-1"><i>
+    <?php echo 'Last modified ' . date('D M d H:i:s T Y', getlastmod()) . '.'; ?>
+    </i></font>
 
 </body>
 </html>
