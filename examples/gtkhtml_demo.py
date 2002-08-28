78637fc026f9ccc88f8f0872601e6d767c3c6202
Kjetil Jacobsen <kjetilja@gmail.com>
2002-08-28 08:44:54 +0000

    reuse curl objects and better error handling for page rendering

--- examples/gtkhtml_demo.py
+++ examples/gtkhtml_demo.py
@@ -18,124 +18,128 @@ NUM_THREADS = 4
 
 
 def about(button):
-	GnomeAbout('GtkHTML Test with PycURL', '0.0',
-		   'May be distributed under the terms of the GPL2',
-		   ['Original code by Matt Wilson, modified by Kjetil Jacobsen'],
-		   ('This is a useless application demonstrating the\n'
-                    'GtkHTML widget with python and PycURL.')).show()
+    GnomeAbout('GtkHTML Test with PycURL', '',
+                'License GPL2',
+                ['Original code by Matt Wilson, modified by Kjetil Jacobsen'],
+                ('This is a useless application demonstrating the '
+                'GtkHTML widget with Python and PycURL.')).show()
+
 
 class WorkerThread(threading.Thread):
-	def __init__(self, queue, render):
-		threading.Thread.__init__(self)
-		self.queue = queue
-		self.render = render
-		
-	def run(self):
-		while 1:
-			url, handle = self.queue.get()
-			if url == None:
-				raise SystemExit
-			b = cStringIO.StringIO()
-			curl = pycurl.Curl()
-			curl.setopt(pycurl.WRITEFUNCTION, b.write)
-			curl.setopt(pycurl.FOLLOWLOCATION, 1)
-			curl.setopt(pycurl.MAXREDIRS, 5)
-			curl.setopt(pycurl.URL, url)
-			try:
-				curl.perform()
-			except:
-				print 'Error retrieving', url
-			curl.close()
-			self.render.append((b, handle))
+
+    def __init__(self, queue, render):
+        threading.Thread.__init__(self)
+        self.queue = queue      # download request queue
+        self.render = render    # render output queue
+
+    def run(self):
+        curl = pycurl.Curl()
+        curl.setopt(pycurl.FOLLOWLOCATION, 1)
+        curl.setopt(pycurl.MAXREDIRS, 5)
+        while 1:
+            url, handle = self.queue.get()
+            if url == None:
+                curl.close()
+                raise SystemExit
+            b = cStringIO.StringIO()
+            curl.setopt(pycurl.WRITEFUNCTION, b.write)
+            curl.setopt(pycurl.URL, url)
+            try:
+                curl.perform()
+            except:
+                b.close()
+                b = None
+                print 'Error retrieving', url
+            self.render.append((b, handle))
 
 
 class HtmlWindow(GtkHTML):
-	def __init__(self):
-		GtkHTML.__init__(self)
-		self.queue = Queue.Queue()
-		self.render = []
-		self.threads = []
-		for num_threads in range(NUM_THREADS):
-			t = WorkerThread(self.queue, self.render)
-			t.start()
-			self.threads.append(t)
 
-	def mainquit(self, *args):
-		for t in self.threads:
-			t.queue.put((None, None))
-		mainquit()
-		
-	def load_url(self, html, url):
-		self.num_obj = 0
-		if history: url = urllib.basejoin(history[-1], url)
-		history.append(url)
+    def __init__(self):
+        GtkHTML.__init__(self)
+        self.queue = Queue.Queue()
+        self.render = []
+        self.threads = []
+        for num_threads in range(NUM_THREADS):
+            t = WorkerThread(self.queue, self.render)
+            t.start()
+            self.threads.append(t)
 
-		html.load_empty()
-		handle = html.begin()
-		self.request_url(html, url, handle)
-		while self.num_obj > 0:
-			if len(self.render) == 0:
-				mainiteration(0)
-				continue
-			self.num_obj -= 1
-			buf, handle = self.render.pop(0)
-			html.write(handle, buf.getvalue())
-			html.end(handle, HTML_STREAM_OK)
-			buf.close()
+    def mainquit(self, *args):
+        for t in self.threads:
+            t.queue.put((None, None))
+        mainquit()
 
-	def submit(self, html, method, path, params):
-		print 'Submit is not supported yet'
-		print method, path, params
+    def load_url(self, html, url):
+        self.num_obj = 0
+        if history: url = urllib.basejoin(history[-1], url)
+        history.append(url)
 
-	def request_url(self, html, url, handle):
-		url = urllib.basejoin(history[-1], url)
-		print "Requesting URL: ", url
-		self.queue.put((url, handle))
-		self.num_obj += 1
+        html.load_empty()
+        handle = html.begin()
+        self.request_url(html, url, handle)
+        while self.num_obj > 0:
+            if len(self.render) == 0:
+                mainiteration(0)
+                continue
+            self.num_obj -= 1
+            buf, handle = self.render.pop(0)
+            if buf != None:
+                html.write(handle, buf.getvalue())
+                buf.close()
+            html.end(handle, HTML_STREAM_OK)
 
-	def entry_activate(self, entry, html):
-		url = entry.get_text()
-		self.load_url(html, url)
-		del forward[:]
+    def submit(self, html, method, path, params):
+        print 'Submit is not supported yet'
+        print method, path, params
 
-	def do_back(self, _b):
-		forward.append(history[-1])
-		del history[-1]
-		url = history[-1]
-		del history[-1]
-		self.load_url(html, url)
+    def request_url(self, html, url, handle):
+        url = urllib.basejoin(history[-1], url)
+        print "Requesting URL: ", url
+        self.queue.put((url, handle))
+        self.num_obj += 1
 
-	def do_forward(self, _b):
-		if len(forward) == 0: return
-		self.load_url(html, forward[-1])
-		del forward[-1]
+    def entry_activate(self, entry, html):
+        url = entry.get_text()
+        self.load_url(html, url)
+        del forward[:]
 
-	def do_reload(self, _b):
-		if len(history) == 0: return
-		url = history[-1]
-		del history[-1]
-		self.load_url(html, url)
-	
+    def do_back(self, _b):
+        forward.append(history[-1])
+        del history[-1]
+        url = history[-1]
+        del history[-1]
+        self.load_url(html, url)
+
+    def do_forward(self, _b):
+        if len(forward) == 0: return
+        self.load_url(html, forward[-1])
+        del forward[-1]
+
+    def do_reload(self, _b):
+        if len(history) == 0: return
+        url = history[-1]
+        del history[-1]
+        self.load_url(html, url)
+
 
 html = HtmlWindow()
 
 file_menu = [
-	UIINFO_ITEM_STOCK('Quit', None, html.mainquit, STOCK_MENU_QUIT),
+    UIINFO_ITEM_STOCK('Quit', None, html.mainquit, STOCK_MENU_QUIT),
 ]
 help_menu = [
-	UIINFO_ITEM_STOCK('About...', None, about, STOCK_MENU_ABOUT),
+    UIINFO_ITEM_STOCK('About...', None, about, STOCK_MENU_ABOUT),
 ]
 menus = [
-	UIINFO_SUBTREE('File', file_menu),
-	UIINFO_SUBTREE('Help', help_menu)
+    UIINFO_SUBTREE('File', file_menu),
+    UIINFO_SUBTREE('Help', help_menu)
 ]
 
 toolbar = [
-	UIINFO_ITEM_STOCK('Back', 'Previous page', html.do_back, STOCK_PIXMAP_BACK),
-	UIINFO_ITEM_STOCK('Forward', 'Next page', html.do_forward,
-			  STOCK_PIXMAP_FORWARD),
-	UIINFO_ITEM_STOCK('Reload', 'Reload current page', html.do_reload,
-			  STOCK_PIXMAP_REFRESH)
+    UIINFO_ITEM_STOCK('Back', 'Previous page', html.do_back, STOCK_PIXMAP_BACK),
+    UIINFO_ITEM_STOCK('Forward', 'Next page', html.do_forward, STOCK_PIXMAP_FORWARD),
+    UIINFO_ITEM_STOCK('Reload', 'Reload current page', html.do_reload, STOCK_PIXMAP_REFRESH)
 ]
 
 
