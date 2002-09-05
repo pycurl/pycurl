eb689b0ecefc687429d721b454c48b9c05716365
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-09-05 14:32:10 +0000

    Updated.

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -4,55 +4,64 @@ $version = "7.9.8.4";
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
+
 <head>
-
   <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
-  <title>PycURL</title>
-
+  <title>PycURL Home Page</title>
   <meta name="author" content="Kjetil Jacobsen">
-
   <meta name="description" content="Homepage">
 </head>
+
 <body bgcolor="#ffffff" link="#0000ee" vlink="#551a8b" alink="#0000ee">
+
 <h1><font face="Courier New, Courier, mono">PYCURL <?php echo $version ?></font></h1>
+
+<p>
 &gt;&gt;&gt; import pycurl<br>
-&gt;&gt;&gt; print pycurl.__doc__<br>
-<br>
-PycURL is a Python module interface to the <a
-href="http://curl.haxx.se/">cURL</a> library.<br>
+&gt;&gt;&gt; print pycurl.__doc__
+</p>
+
+<p>
+PycURL is a Python module interface to the
+<a href="http://curl.haxx.se/">cURL</a> library.
+<br />
 PycURL can be used to fetch documents identified by a URI
-from within a Python program.<br>
-<br>
+from within a Python program.
+</p>
+
+<p>
 <a href="download/pycurl-7.9.8.4.tar.gz">Download</a>
 PycURL sources version <?php echo $version ?> (Sep 04 2002) or
-try the code from 
+try the code from
 <a href="http://sourceforge.net/cvs/?group_id=28236">the CVS repository</a>.
-<br>
-<br>
+</p>
+
+<p>
 For Win32 users there's also an
-<a href="download/pycurl-7.9.8.3.win32-py2.2.exe">installer with a precompiled module</a>.
+<a href="download/pycurl-7.9.8.4.win32-py2.2.exe">installer with a precompiled module</a>.
 You still should download the sources because of the test and example programs.
-<br>
-<br>
+</p>
+
+<p>
 Official Debian packages are available for
 <a href="http://packages.debian.org/python2.1-pycurl">Python 2.1</a>
 and
 <a href="http://packages.debian.org/python2.2-pycurl">Python 2.2</a>.
-<br>
-<br>
+</p>
+
+<p>
 Finally you can get older versions from the
 <a href="download/?M=D">download area</a>.
-<br>
+</p>
 
-    <br />
     <br />
     <hr />
     <div align="right">
       <table align="right">
         <tr align="right">
           <td><a href="http://curl.haxx.se/"><img
-          src="http://curl.haxx.se/pix/powered_by_curl.gif" alt=""
-          width="88" height="31" border="0" /></a>
+          src="http://curl.haxx.se/pix/powered_by_curl.gif"
+          width="88" height="31" border="0" alt="" /></a>
           </td>
 
           <td><a href="http://sourceforge.net/"><img
@@ -64,7 +73,7 @@ Finally you can get older versions from the
     </div>
 
     <font size="-1"><i>
-    <?php echo 'Last modified ' . date('D M d H:i:s T Y', getlastmod()) . '.'; ?>
+      <?php echo 'Last modified ' . date('D M d H:i:s T Y', getlastmod()) . '.'; ?>
     </i></font>
 
 </body>
