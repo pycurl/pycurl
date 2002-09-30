91836f003695c8cebeb66343f5d359b66ad59ceb
Kjetil Jacobsen <kjetilja@gmail.com>
2002-09-30 12:30:44 +0000

    add text output which explains what happened

--- tests/test_multi5.py
+++ tests/test_multi5.py
@@ -51,3 +51,7 @@ c3.body.close()
 c1.close()
 c2.close()
 c3.close()
+print 'http://www.python.org is in file doc1'
+print 'http://curl.haxx.se is in file doc2'
+print 'http://slashdot.org is in file doc3'
+
