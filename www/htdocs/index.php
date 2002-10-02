c5108e0b94705fd09775769a4bc83b994b8bbd1c
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2002-10-02 19:48:00 +0000

    Updated.

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -6,10 +6,10 @@ $version = "7.10";
 <html>
 
 <head>
-  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
   <title>PycURL Home Page</title>
-  <meta name="author" content="Kjetil Jacobsen">
-  <meta name="description" content="Homepage">
+  <meta name="author" content="Kjetil Jacobsen" />
+  <meta name="description" content="PycURL Homepage" />
 </head>
 
 <body bgcolor="#ffffff" link="#0000ee" vlink="#551a8b" alink="#0000ee">
@@ -17,7 +17,7 @@ $version = "7.10";
 <h1><font face="Courier New, Courier, mono">PYCURL <?php echo $version ?></font></h1>
 
 <p>
-&gt;&gt;&gt; import pycurl<br>
+&gt;&gt;&gt; import pycurl<br />
 &gt;&gt;&gt; print pycurl.__doc__
 </p>
 
@@ -31,23 +31,27 @@ from within a Python program.
 
 <p>
 <a href="download/pycurl-7.10.tar.gz">Download</a>
-PycURL sources version <?php echo $version ?> (Oct 2 2002) or
-try the code from
+PycURL sources version <?php echo $version ?> (Oct 2 2002)
+or try the code from
 <a href="http://sourceforge.net/cvs/?group_id=28236">the CVS repository</a>.
+</p>
+
+<p>
+You can get older versions as well as prebuilt Win32 modules from the
+<a href="download/?M=D">download area</a>.
 </p>
 
 <p>
 Official Debian packages are available for
 <a href="http://packages.debian.org/python2.1-pycurl">Python 2.1</a>
 and
-<a href="http://packages.debian.org/python2.2-pycurl">Python 2.2</a>.<br>  PycURL is also
-in the <a href="http://www.freebsd.org/ports/">ports</a> collection for FreeBSD
-and in the <a href="http://www.netbsd.org/Documentation/software/packages.html">packages</a>
+<a href="http://packages.debian.org/python2.2-pycurl">Python 2.2</a>.
+<br />
+PycURL is also in the
+<a href="http://www.freebsd.org/ports/">ports</a>
+collection for FreeBSD and in the
+<a href="http://www.netbsd.org/Documentation/software/packages.html">packages</a>
 collection for NetBSD.
-</p>
-
-<p>
-You can get older versions as well as prebuilt Win32 modules from the <a href="download/?M=D">download area</a>.
 </p>
 
 <p>
@@ -64,26 +68,28 @@ License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
 </pre>
 </p>
-    <hr />
-    <div align="right">
-      <table align="right">
-        <tr align="right">
-          <td><a href="http://curl.haxx.se/"><img
-          src="http://curl.haxx.se/pix/powered_by_curl.gif"
-          width="88" height="31" border="0" alt="" /></a>
-          </td>
 
-          <td><a href="http://sourceforge.net/"><img
-          src="http://sourceforge.net/sflogo.php?group_id=28236&amp;type=1"
-          width="88" height="31" border="0" alt="" /></a>
-          </td>
-        </tr>
-      </table>
-    </div>
 
-    <font size="-1"><i>
-      <?php echo 'Last modified ' . date('D M d H:i:s T Y', getlastmod()) . '.'; ?>
-    </i></font>
+<hr />
+<div align="right">
+  <table align="right">
+    <tr align="right">
+      <td><a href="http://curl.haxx.se/"><img
+      src="http://curl.haxx.se/pix/powered_by_curl.gif"
+      width="88" height="31" border="0" alt="" /></a>
+      </td>
+
+      <td><a href="http://sourceforge.net/"><img
+      src="http://sourceforge.net/sflogo.php?group_id=28236&amp;type=1"
+      width="88" height="31" border="0" alt="" /></a>
+      </td>
+    </tr>
+  </table>
+</div>
+
+<font size="-1"><i>
+  <?php echo 'Last modified ' . date('D M d H:i:s T Y', getlastmod()) . '.'; ?>
+</i></font>
 
 </body>
 </html>
