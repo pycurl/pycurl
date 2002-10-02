547e28e65c1bf48322a6000a6b04d5cf4db7433e
Kjetil Jacobsen <kjetilja@gmail.com>
2002-10-02 10:47:30 +0000

    up

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -59,8 +59,6 @@ PycURL is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
-
-A full copy of the LGPL license is included in the file COPYING.
 </pre>
 </p>
     <hr />
