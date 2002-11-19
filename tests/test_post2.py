4a4436c185622f386cc7aa9b513641e4d5febdd5
Kjetil Jacobsen <kjetilja@gmail.com>
2002-11-19 13:37:27 +0000

    post to a url that works

--- tests/test_post2.py
+++ tests/test_post2.py
@@ -8,7 +8,7 @@ import pycurl
 pf = ['field1=this is a test using httppost & stuff', 'field2=value2']
 
 c = pycurl.Curl()
-c.setopt(c.URL, 'http://pycurl.sourceforge.net/tests/testpostvars.php')
+c.setopt(c.URL, 'http://www.contactor.se/~dast/postit.cgi')
 c.setopt(c.HTTPPOST, pf)
 c.setopt(c.VERBOSE, 1)
 c.perform()
