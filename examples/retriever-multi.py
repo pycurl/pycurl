8c95fae80f42ac49eca982d493e89f43e0019a60
Kjetil Jacobsen <kjetilja@gmail.com>
2003-01-14 11:34:28 +0000

    added comments, set nosignal and cleaned up some of the code

--- examples/retriever-multi.py
+++ examples/retriever-multi.py
@@ -5,6 +5,13 @@
 
 import sys
 import pycurl
+try:
+    import signal
+    from signal import SIGPIPE, SIG_IGN
+    signal.signal(signal.SIGPIPE, signal.SIG_IGN)
+except ImportError:
+    pass
+
 assert sys.version[:3] >= "2.2", "requires Python 2.2 or better"
 
 try:
@@ -15,14 +22,16 @@ except:
     raise SystemExit
 
 num_conn = min(num_conn, len(urls))
-assert 1 <= num_conn <= 10000
+assert 1 <= num_conn <= 10000, "invalid number of connections"
 
+# Make a queue with (url, filename) tuples
 fileno = 0
 queue = []
 for u in urls:
     queue.append((u, "data_%d" % fileno))
     fileno += 1
 
+# Preallocate a list of curl objects 
 freelist = []
 for c in range(num_conn):
     curl = pycurl.Curl()
@@ -30,6 +39,7 @@ for c in range(num_conn):
     curl.setopt(pycurl.FOLLOWLOCATION, 1)
     curl.setopt(pycurl.MAXREDIRS, 5)
     curl.setopt(pycurl.CONNECTTIMEOUT, 30)
+    curl.setopt(pycurl.NOSIGNAL, 1)
     freelist.append(curl)
 
 processed = 0
@@ -37,6 +47,7 @@ curls = {}
 multi = pycurl.CurlMulti()
 
 while processed < len(urls):
+    # If there is an url to process and a free curl object, add to multi stack
     while queue and freelist:
         u, n = queue.pop(0)
         f = open(n, "wb")
@@ -45,10 +56,12 @@ while processed < len(urls):
         c.setopt(pycurl.WRITEDATA, f)
         curls[c] = f
         multi.add_handle(c)
+    # Run the internal curl state machine for the multi stack
     while 1:
         ret, num_handles = multi.perform()
         if ret != pycurl.E_CALL_MULTI_PERFORM:
             break
+    # Check for curl objects which have terminated, and add them to the freelist
     while 1:
         num_q, handles = multi.info_read(num_conn)
         for h in handles:
@@ -59,13 +72,14 @@ while processed < len(urls):
         if num_q == 0:
             break
 
+# Cleanup
 for c in curls:
     c.close()
     multi.remove_handle(c)
     freelist.remove(c)
+del c
 multi.close()
 
 del curls
 del freelist
 del queue
-
