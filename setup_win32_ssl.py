ddf03e432bf9acb83a6fb34c887534cc2698da5d
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2003-02-25 19:05:51 +0000

    Updated.

--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -8,7 +8,7 @@ assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
 
 
 CURL_DIR = r"c:\src\build\curl-7.10.4-ssl"
-OPENSSL_DIR = r"c:\src\build\openssl-0.9.6h"
+OPENSSL_DIR = r"c:\src\build\openssl-0.9.6i"
 ZLIB_DIR = r"c:\src\build\zlib-1.1.4"
 sys.argv.insert(1, "--curl-dir=" + CURL_DIR)
 
