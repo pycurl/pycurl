d613fa359ab58bbb63812f51d0c84a6c16add90c
Kjetil Jacobsen <kjetilja@gmail.com>
2004-08-17 08:58:22 +0000

    bump version numbers

--- setup_win32_ssl.py
+++ setup_win32_ssl.py
@@ -7,7 +7,7 @@ import os, sys, string
 assert sys.platform == "win32", "Only for building on Win32 with SSL and zlib"
 
 
-CURL_DIR = r"c:\src\build\curl-7.12.1-ssl"
+CURL_DIR = r"c:\src\build\curl-7.12.1.1-ssl"
 OPENSSL_DIR = r"c:\src\build\openssl-0.9.7d"
 sys.argv.insert(1, "--curl-dir=" + CURL_DIR)
 
