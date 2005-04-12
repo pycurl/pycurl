0f90ea0c1b05583b8f12e8e15d61572b41f30729
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2005-04-12 03:37:25 +0000

    Updates.

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -1,7 +1,7 @@
 <?php // vi:ts=4:et
 echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n";
 $version = "7.13.2";
-$version_date = "Mar 6 2005";
+$version_date = "Apr 6 2005";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
