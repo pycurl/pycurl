4cad1bca77011b1208b6e47808e84dabfddb124b
Oleg Pudeyev <oleg@bsdpower.com>
2013-02-28 00:14:03 -0500

    Python 3 compatibility: StringIO (src)

--- python/curl/__init__.py
+++ python/curl/__init__.py
@@ -10,7 +10,10 @@ import os, sys, urllib, exceptions, mimetools, pycurl
 try:
     from cStringIO import StringIO
 except ImportError:
-    from StringIO import StringIO
+    try:
+        from StringIO import StringIO
+    except ImportError:
+        from io import StringIO
 
 try:
     import signal
