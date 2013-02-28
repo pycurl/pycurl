f0a33e3898b71df9b6c99fc10e7a103a7d828378
Oleg Pudeyev <oleg@bsdpower.com>
2013-02-28 00:11:58 -0500

    Python 3 compatibility: StringIO (examples)

--- examples/xmlrpc_curl.py
+++ examples/xmlrpc_curl.py
@@ -14,7 +14,10 @@ except ImportError:
 try:
     from cStringIO import StringIO
 except ImportError:
-    from StringIO import StringIO
+    try:
+        from StringIO import StringIO
+    except ImportError:
+        from io import StringIO
 import xmlrpclib, pycurl
 
 
