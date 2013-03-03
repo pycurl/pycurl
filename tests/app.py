24b89059f9e46c2a2e7f197e36a4d42fce23ee75
Oleg Pudeyev <oleg@bsdpower.com>
2013-03-03 12:55:42 -0500

    Port multi2 test

--- tests/app.py
+++ tests/app.py
@@ -5,3 +5,11 @@ app = bottle.Bottle()
 @app.route('/success')
 def ok():
     return 'success'
+
+@app.route('/status/403')
+def forbidden():
+    bottle.abort(403, 'forbidden')
+
+@app.route('/status/404')
+def not_found():
+    bottle.abort(404, 'not found')
--- tests/multi_test.py
+++ tests/multi_test.py
@@ -11,12 +11,15 @@ from . import util
 
 setup_module_1, teardown_module_1 = runwsgi.app_runner_setup((app.app, 8380))
 setup_module_2, teardown_module_2 = runwsgi.app_runner_setup((app.app, 8381))
+setup_module_3, teardown_module_3 = runwsgi.app_runner_setup((app.app, 8382))
 
 def setup_module(mod):
     setup_module_1(mod)
     setup_module_2(mod)
+    setup_module_3(mod)
 
 def teardown_module(mod):
+    teardown_module_3(mod)
     teardown_module_2(mod)
     teardown_module_1(mod)
 
@@ -54,3 +57,54 @@ class MultiTest(unittest.TestCase):
         
         self.assertEqual('success', io1.getvalue())
         self.assertEqual('success', io2.getvalue())
+    
+    def test_multi_status_codes(self):
+        # init
+        m = pycurl.CurlMulti()
+        m.handles = []
+        urls = [
+            'http://localhost:8380/success',
+            'http://localhost:8381/status/403',
+            'http://localhost:8382/status/404',
+        ]
+        for url in urls:
+            c = pycurl.Curl()
+            # save info in standard Python attributes
+            c.url = url.rstrip()
+            c.body = util.StringIO()
+            c.http_code = -1
+            m.handles.append(c)
+            # pycurl API calls
+            c.setopt(c.URL, c.url)
+            c.setopt(c.WRITEFUNCTION, c.body.write)
+            m.add_handle(c)
+
+        # get data
+        num_handles = len(m.handles)
+        while num_handles:
+            while 1:
+                ret, num_handles = m.perform()
+                if ret != pycurl.E_CALL_MULTI_PERFORM:
+                    break
+            # currently no more I/O is pending, could do something in the meantime
+            # (display a progress bar, etc.)
+            m.select(0.1)
+
+        # close handles
+        for c in m.handles:
+            # save info in standard Python attributes
+            c.http_code = c.getinfo(c.HTTP_CODE)
+            # pycurl API calls
+            m.remove_handle(c)
+            c.close()
+        m.close()
+
+        # check result
+        self.assertEqual('success', m.handles[0].body.getvalue())
+        self.assertEqual(200, m.handles[0].http_code)
+        # bottle generated response body
+        assert 'Error 403: Forbidden' in m.handles[1].body.getvalue()
+        self.assertEqual(403, m.handles[1].http_code)
+        # bottle generated response body
+        assert 'Error 404: Not Found' in m.handles[2].body.getvalue()
+        self.assertEqual(404, m.handles[2].http_code)
