757001eef19399cb50cbb3650871da3602620c46
Oleg Pudeyev <oleg@bsdpower.com>
2013-03-06 01:29:59 -0500

    Store urlencode result in a local variable, otherwise things break in a highly weird way

--- tests/post_test.py
+++ tests/post_test.py
@@ -40,11 +40,33 @@ class PostTest(unittest.TestCase):
     
     def urlencode_and_check(self, pf):
         self.curl.setopt(pycurl.URL, 'http://localhost:8380/postfields')
-        self.curl.setopt(pycurl.POSTFIELDS, urllib_parse.urlencode(pf))
+        postfields = urllib_parse.urlencode(pf)
+        self.curl.setopt(pycurl.POSTFIELDS, postfields)
+        
+        # But directly passing urlencode result into setopt call:
+        #self.curl.setopt(pycurl.POSTFIELDS, urllib_parse.urlencode(pf))
+        # produces:
+        # {'\x00\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00\x00': ''}
+        # Traceback (most recent call last):
+        #   File "/usr/local/bin/bottle.py", line 744, in _handle
+        #     return route.call(**args)
+        #   File "/usr/local/bin/bottle.py", line 1479, in wrapper
+        #     rv = callback(*a, **ka)
+        #   File "/home/pie/apps/pycurl/tests/app.py", line 21, in postfields
+        #     return json.dumps(dict(bottle.request.forms))
+        #   File "/usr/local/lib/python2.7/json/__init__.py", line 231, in dumps
+        #     return _default_encoder.encode(obj)
+        #   File "/usr/local/lib/python2.7/json/encoder.py", line 201, in encode
+        #     chunks = self.iterencode(o, _one_shot=True)
+        #   File "/usr/local/lib/python2.7/json/encoder.py", line 264, in iterencode
+        #     return _iterencode(o, 0)
+        # UnicodeDecodeError: 'utf8' codec can't decode byte 0x80 in position 4: invalid start byte
+        
         #self.curl.setopt(pycurl.VERBOSE, 1)
         sio = util.StringIO()
         self.curl.setopt(pycurl.WRITEFUNCTION, sio.write)
         self.curl.perform()
+        self.assertEqual(200, self.curl.getinfo(pycurl.HTTP_CODE))
         body = sio.getvalue()
         returned_fields = json.loads(body)
         self.assertEqual(pf, returned_fields)
