d4584a96298dc7dc1195e2df37641fd226dc2d23
Oleg Pudeyev <oleg@bsdpower.com>
2013-03-05 23:35:02 -0500

    Port test_post2.py null byte test

--- tests/post_test.py
+++ tests/post_test.py
@@ -26,18 +26,18 @@ class PostTest(unittest.TestCase):
     
     def test_post_single_field(self):
         pf = {'field1': 'value1'}
-        self.check(pf)
+        self.urlencode_and_check(pf)
     
     def test_post_multiple_fields(self):
         pf = {'field1':'value1', 'field2':'value2 with blanks', 'field3':'value3'}
-        self.check(pf)
+        self.urlencode_and_check(pf)
     
     def test_post_fields_with_ampersand(self):
         pf = {'field1':'value1', 'field2':'value2 with blanks and & chars',
               'field3':'value3'}
-        self.check(pf)
+        self.urlencode_and_check(pf)
     
-    def check(self, pf):
+    def urlencode_and_check(self, pf):
         self.curl.setopt(pycurl.URL, 'http://localhost:8380/postfields')
         self.curl.setopt(pycurl.POSTFIELDS, urllib_parse.urlencode(pf))
         #self.curl.setopt(pycurl.VERBOSE, 1)
@@ -47,3 +47,23 @@ class PostTest(unittest.TestCase):
         body = sio.getvalue()
         returned_fields = json.loads(body)
         self.assertEqual(pf, returned_fields)
+    
+    def test_post_with_null_byte(self):
+        send = [
+            ('field3', (pycurl.FORM_CONTENTS, 'this is wei\000rd, but null-bytes are okay'))
+        ]
+        expect = {
+            'field3': 'this is wei\000rd, but null-bytes are okay',
+        }
+        self.check_post(send, expect)
+    
+    def check_post(self, send, expect):
+        self.curl.setopt(pycurl.URL, 'http://localhost:8380/postfields')
+        self.curl.setopt(pycurl.HTTPPOST, send)
+        #self.curl.setopt(pycurl.VERBOSE, 1)
+        sio = util.StringIO()
+        self.curl.setopt(pycurl.WRITEFUNCTION, sio.write)
+        self.curl.perform()
+        body = sio.getvalue()
+        returned_fields = json.loads(body)
+        self.assertEqual(expect, returned_fields)
