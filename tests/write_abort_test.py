208a1109e618307d61034c18c8a6f89f8de2cf25
Oleg Pudeyev <oleg@bsdpower.com>
2013-03-11 23:48:10 -0400

    setup/teardown consistent with other tests

--- tests/write_abort_test.py
+++ tests/write_abort_test.py
@@ -9,10 +9,10 @@ import unittest
 
 class WriteAbortTest(unittest.TestCase):
     def setUp(self):
-        pycurl.global_init(pycurl.GLOBAL_DEFAULT)
+        self.curl = pycurl.Curl()
 
     def tearDown(self):
-        pycurl.global_cleanup()
+        self.curl.close()
 
     def test_write_abort(self):
         def write_cb(_):
@@ -20,16 +20,13 @@ class WriteAbortTest(unittest.TestCase):
             return -1
 
         # download the script itself through the file:// protocol into write_cb
-        c = pycurl.Curl()
-        c.setopt(pycurl.URL, 'file://' + os.path.abspath(sys.argv[0]))
-        c.setopt(pycurl.WRITEFUNCTION, write_cb)
+        self.curl.setopt(pycurl.URL, 'file://' + os.path.abspath(sys.argv[0]))
+        self.curl.setopt(pycurl.WRITEFUNCTION, write_cb)
         try:
-            c.perform()
+            self.curl.perform()
         except pycurl.error, (err, msg):
             # we expect pycurl.E_WRITE_ERROR as the response
             assert pycurl.E_WRITE_ERROR == err
 
         # no additional errors should be reported
         assert not hasattr(sys, 'last_value')
-
-        c.close()
