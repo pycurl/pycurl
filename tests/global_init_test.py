5c734f6276a17e621248b44f423783547c4b14c1
Oleg Pudeyev <oleg@bsdpower.com>
2013-05-30 15:27:02 -0400

    Use nose test skipping functionality

--- tests/global_init_test.py
+++ tests/global_init_test.py
@@ -5,6 +5,7 @@
 import pycurl
 import unittest
 import nose.tools
+import nose.plugins.skip
 
 from . import util
 
@@ -17,10 +18,12 @@ class GlobalInitTest(unittest.TestCase):
     def test_global_init_ack_eintr(self):
         # the GLOBAL_ACK_EINTR flag was introduced in libcurl-7.30, but can also
         # be backported for older versions of libcurl at the distribution level
-        if not util.pycurl_version_less_than(7, 30) or hasattr(pycurl, 'GLOBAL_ACK_EINTR'):
-            # initialize libcurl with the GLOBAL_ACK_EINTR flag
-            pycurl.global_init(pycurl.GLOBAL_ACK_EINTR)
-            pycurl.global_cleanup()
+        if util.pycurl_version_less_than(7, 30) or hasattr(pycurl, 'GLOBAL_ACK_EINTR'):
+            raise nose.plugins.skip.SkipTest('libcurl < 7.30.0 or no GLOBAL_ACK_EINTR')
+        
+        # initialize libcurl with the GLOBAL_ACK_EINTR flag
+        pycurl.global_init(pycurl.GLOBAL_ACK_EINTR)
+        pycurl.global_cleanup()
     
     @nose.tools.raises(ValueError)
     def test_global_init_bogus(self):
