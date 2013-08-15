63f86c88100f99ea0241889ceda9b3668f1da242
Oleg Pudeyev <oleg@bsdpower.com>
2013-08-15 16:44:32 -0400

    Do not run resolve test on curls < 7.21.3 which do not support it

--- tests/resolve_test.py
+++ tests/resolve_test.py
@@ -2,9 +2,11 @@
 
 import pycurl
 import unittest
+import nose.plugins.skip
 
 from . import app
 from . import runwsgi
+from . import util
 
 setup_module, teardown_module = runwsgi.app_runner_setup((app.app, 8380))
 
@@ -16,6 +18,9 @@ class ResolveTest(unittest.TestCase):
         self.curl.close()
     
     def test_resolve(self):
+        if util.pycurl_version_less_than(7, 21, 3) and not hasattr(pycurl, 'RESOLVE'):
+            raise nose.plugins.skip.SkipTest('libcurl < 7.21.3 or no RESOLVE')
+        
         self.curl.setopt(pycurl.URL, 'http://p.localhost:8380/success')
         self.curl.setopt(pycurl.RESOLVE, ['p.localhost:8380:127.0.0.1'])
         self.curl.perform()
