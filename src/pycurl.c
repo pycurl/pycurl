253a5ce9a94545823a23e0c7202f8b473db83a21
Oleg Pudeyev <oleg@bsdpower.com>
2013-09-25 04:35:11 -0400

    Update threaded code to be conditional

--- src/pycurl.c
+++ src/pycurl.c
@@ -2476,7 +2476,9 @@ do_curl_pause(CurlObject *self, PyObject *args)
 {
     int bitmask;
     CURLcode res;
+#ifdef WITH_THREAD
     PyThreadState *saved_state;
+#endif
 
     if (!PyArg_ParseTuple(args, "i:pause", &bitmask)) {
         return NULL;
@@ -2485,20 +2487,24 @@ do_curl_pause(CurlObject *self, PyObject *args)
         return NULL;
     }
 
+#ifdef WITH_THREAD
     /* Save handle to current thread (used as context for python callbacks) */
     saved_state = self->state;
-    self->state = PyThreadState_Get();
-    assert(self->state != NULL);
+    PYCURL_BEGIN_ALLOW_THREADS
 
-    Py_BEGIN_ALLOW_THREADS
     /* We must allow threads here because unpausing a handle can cause
        some of its callbacks to be invoked immediately, from inside
        curl_easy_pause() */
+#endif
+
     res = curl_easy_pause(self->handle, bitmask);
-    Py_END_ALLOW_THREADS
+
+#ifdef WITH_THREAD
+    PYCURL_END_ALLOW_THREADS
 
     /* Restore the thread-state to whatever it was on entry */
     self->state = saved_state;
+#endif
 
     if (res != CURLE_OK) {
         CURLERROR_MSG("pause/unpause failed");
