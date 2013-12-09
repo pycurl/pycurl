fd5f3875d0d7e0fdb7b7ef33a94cf50d1d2b5fa4
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-09 00:05:58 -0500

    Add a test for writing to StringIO which is now different and does not work

--- tests/write_to_stringio_test.py
+++ tests/write_to_stringio_test.py
@@ -4,6 +4,7 @@
 
 import pycurl
 import unittest
+import sys
 
 from . import appmanager
 from . import util
@@ -23,3 +24,18 @@ class WriteToStringioTest(unittest.TestCase):
         self.curl.setopt(pycurl.WRITEFUNCTION, sio.write)
         self.curl.perform()
         self.assertEqual('success', sio.getvalue().decode())
+    
+    @util.only_python3
+    def test_write_to_stringio(self):
+        self.curl.setopt(pycurl.URL, 'http://localhost:8380/success')
+        # stringio in python 3
+        sio = util.StringIO()
+        self.curl.setopt(pycurl.WRITEFUNCTION, sio.write)
+        try:
+            self.curl.perform()
+            
+            self.fail('Should have received a write error')
+        except pycurl.error:
+            err, msg = sys.exc_info()[1].args
+            # we expect pycurl.E_WRITE_ERROR as the response
+            assert pycurl.E_WRITE_ERROR == err
