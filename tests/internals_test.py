268ad8247bed3b78d8b60ad82659c8392e10c6d0
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-11 10:04:14 -0500

    Move refcounting tests to memory management test case

--- tests/internals_test.py
+++ tests/internals_test.py
@@ -10,7 +10,6 @@ try:
 except ImportError:
     cPickle = None
 import pickle
-import gc
 import copy
 
 class InternalsTest(unittest.TestCase):
@@ -163,66 +162,3 @@ class InternalsTest(unittest.TestCase):
             else:
                 assert 0, "No exception when trying to pickle a CurlMulti handle via cPickle"
             del m, fp, p
-
-    # /***********************************************************************
-    # // test refcounts
-    # ************************************************************************/
-
-    # basic check of reference counting (use a memory checker like valgrind)
-    def test_reference_counting(self):
-        c = pycurl.Curl()
-        m = pycurl.CurlMulti()
-        m.add_handle(c)
-        del m
-        m = pycurl.CurlMulti()
-        c.close()
-        del m, c
-    
-    def test_cyclic_gc(self):
-        gc.collect()
-        c = pycurl.Curl()
-        c.m = pycurl.CurlMulti()
-        c.m.add_handle(c)
-        # create some nasty cyclic references
-        c.c = c
-        c.c.c1 = c
-        c.c.c2 = c
-        c.c.c3 = c.c
-        c.c.c4 = c.m
-        c.m.c = c
-        c.m.m = c.m
-        c.m.c = c
-        # delete
-        gc.collect()
-        flags = gc.DEBUG_COLLECTABLE | gc.DEBUG_UNCOLLECTABLE
-        # python 3 has no DEBUG_OBJECTS
-        #if hasattr(gc, 'DEBUG_OBJECTS'):
-            #flags |= gc.DEBUG_OBJECTS
-        #if opts.verbose >= 1:
-            #flags = flags | gc.DEBUG_STATS
-        #gc.set_debug(flags)
-        gc.collect()
-        ##print gc.get_referrers(c)
-        ##print gc.get_objects()
-        #if opts.verbose >= 1:
-            #print("Tracked objects:", len(gc.get_objects()))
-        c_id = id(c)
-        # The `del' below should delete these 4 objects:
-        #   Curl + internal dict, CurlMulti + internal dict
-        del c
-        gc.collect()
-        objects = gc.get_objects()
-        for object in objects:
-            assert id(object) != c_id
-        #if opts.verbose >= 1:
-            #print("Tracked objects:", len(gc.get_objects()))
-    
-    def test_refcounting_bug_in_reset(self):
-        try:
-            range_generator = xrange
-        except NameError:
-            range_generator = range
-        # Ensure that the refcounting error in "reset" is fixed:
-        for i in range_generator(100000):
-            c = pycurl.Curl()
-            c.reset()
