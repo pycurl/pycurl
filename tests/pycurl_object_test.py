79188e53914ca1176ea9e8f771aa84bab05d8fbe
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-14 15:00:43 -0500

    Check multi and share objects for attribute assignment behavior

--- tests/pycurl_object_test.py
+++ tests/pycurl_object_test.py
@@ -13,20 +13,55 @@ class PycurlObjectTest(unittest.TestCase):
     def tearDown(self):
         self.curl.close()
     
-    def test_set_attribute(self):
-        assert not hasattr(self.curl, 'attr')
-        self.curl.attr = 1
-        assert hasattr(self.curl, 'attr')
+    def test_set_attribute_curl(self):
+        self.instantiate_and_check(self.check_set_attribute, 'Curl')
     
-    def test_get_attribute(self):
-        assert not hasattr(self.curl, 'attr')
-        self.curl.attr = 1
-        self.assertEqual(1, self.curl.attr)
+    def test_get_attribute_curl(self):
+        self.instantiate_and_check(self.check_get_attribute, 'Curl')
     
-    def test_delete_attribute(self):
-        assert not hasattr(self.curl, 'attr')
-        self.curl.attr = 1
-        self.assertEqual(1, self.curl.attr)
-        assert hasattr(self.curl, 'attr')
-        del self.curl.attr
-        assert not hasattr(self.curl, 'attr')
+    def test_delete_attribute_curl(self):
+        self.instantiate_and_check(self.check_delete_attribute, 'Curl')
+    
+    def test_set_attribute_multi(self):
+        self.instantiate_and_check(self.check_set_attribute, 'CurlMulti')
+    
+    def test_get_attribute_multi(self):
+        self.instantiate_and_check(self.check_get_attribute, 'CurlMulti')
+    
+    def test_delete_attribute_multi(self):
+        self.instantiate_and_check(self.check_delete_attribute, 'CurlMulti')
+    
+    def test_set_attribute_share(self):
+        self.instantiate_and_check(self.check_set_attribute, 'CurlShare')
+    
+    def test_get_attribute_share(self):
+        self.instantiate_and_check(self.check_get_attribute, 'CurlShare')
+    
+    def test_delete_attribute_share(self):
+        self.instantiate_and_check(self.check_delete_attribute, 'CurlShare')
+    
+    def instantiate_and_check(self, fn, cls_name):
+        cls = getattr(pycurl, cls_name)
+        instance = cls()
+        try:
+            fn(instance)
+        finally:
+            instance.close()
+    
+    def check_set_attribute(self, pycurl_obj):
+        assert not hasattr(pycurl_obj, 'attr')
+        pycurl_obj.attr = 1
+        assert hasattr(pycurl_obj, 'attr')
+    
+    def check_get_attribute(self, pycurl_obj):
+        assert not hasattr(pycurl_obj, 'attr')
+        pycurl_obj.attr = 1
+        self.assertEqual(1, pycurl_obj.attr)
+    
+    def check_delete_attribute(self, pycurl_obj):
+        assert not hasattr(pycurl_obj, 'attr')
+        pycurl_obj.attr = 1
+        self.assertEqual(1, pycurl_obj.attr)
+        assert hasattr(pycurl_obj, 'attr')
+        del pycurl_obj.attr
+        assert not hasattr(pycurl_obj, 'attr')
