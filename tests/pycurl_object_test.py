bc46e103b162c80925f4551f1f732248ec2aff24
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-14 17:30:13 -0500

    Check that trying to delete nonexistent attributes raises AttributeError

--- tests/pycurl_object_test.py
+++ tests/pycurl_object_test.py
@@ -25,6 +25,9 @@ class PycurlObjectTest(unittest.TestCase):
     def test_delete_attribute_curl(self):
         self.instantiate_and_check(self.check_delete_attribute, 'Curl')
     
+    def test_delete_missing_attribute_curl(self):
+        self.instantiate_and_check(self.check_delete_missing_attribute, 'Curl')
+    
     def test_set_attribute_multi(self):
         self.instantiate_and_check(self.check_set_attribute, 'CurlMulti')
     
@@ -37,6 +40,9 @@ class PycurlObjectTest(unittest.TestCase):
     def test_delete_attribute_multi(self):
         self.instantiate_and_check(self.check_delete_attribute, 'CurlMulti')
     
+    def test_delete_missing_attribute_curl(self):
+        self.instantiate_and_check(self.check_delete_missing_attribute, 'CurlMulti')
+    
     def test_set_attribute_share(self):
         self.instantiate_and_check(self.check_set_attribute, 'CurlShare')
     
@@ -48,6 +54,9 @@ class PycurlObjectTest(unittest.TestCase):
     
     def test_delete_attribute_share(self):
         self.instantiate_and_check(self.check_delete_attribute, 'CurlShare')
+    
+    def test_delete_missing_attribute_curl(self):
+        self.instantiate_and_check(self.check_delete_missing_attribute, 'CurlShare')
     
     def instantiate_and_check(self, fn, cls_name):
         cls = getattr(pycurl, cls_name)
@@ -81,6 +90,13 @@ class PycurlObjectTest(unittest.TestCase):
         assert hasattr(pycurl_obj, 'attr')
         del pycurl_obj.attr
         assert not hasattr(pycurl_obj, 'attr')
+    
+    def check_delete_missing_attribute(self, pycurl_obj):
+        try:
+            del pycurl_obj.doesnotexist
+            self.fail('Expected an AttributeError exception to be raised')
+        except AttributeError:
+            pass
     
     def test_modify_attribute_curl(self):
         self.check_modify_attribute(pycurl.Curl, 'READFUNC_PAUSE')
