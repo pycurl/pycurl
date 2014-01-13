14edd084d4a7c5323a795139eb43141f3781ebc4
Oleg Pudeyev <oleg@bsdpower.com>
2014-01-12 19:43:20 -0500

    Test coverage for pulling ssl library out of --libs and --static-libs of curl-config

---
+++ tests/setup_test.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vi:ts=4:et
+
+import setup as pycurl_setup
+import unittest
+import os, os.path
+import nose.plugins.skip
+
+try:
+    import functools
+except ImportError:
+    import functools_backport as functools
+
+def using_curl_config(path):
+    path = os.path.join(os.path.dirname(__file__), 'fake-curl', path)
+    def decorator(fn):
+        @functools.wraps(fn)
+        def decorated(*args, **kwargs):
+            wasset = os.environ.has_key('PYCURL_CURL_CONFIG')
+            old = os.environ.get('PYCURL_CURL_CONFIG')
+            os.environ['PYCURL_CURL_CONFIG'] = path
+            try:
+                return fn(*args, **kwargs)
+            finally:
+                if wasset:
+                    os.environ['PYCURL_CURL_CONFIG'] = old
+                else:
+                    del os.environ['PYCURL_CURL_CONFIG']
+        return decorated
+    return decorator
+
+class SetupTest(unittest.TestCase):
+    def test_sanity_check(self):
+        config = pycurl_setup.ExtensionConfiguration()
+        # we should link against libcurl, one would expect
+        assert 'curl' in config.libraries
+    
+    @using_curl_config('curl-config-empty')
+    def test_no_ssl(self):
+        config = pycurl_setup.ExtensionConfiguration()
+        # do not expect anything to do with ssl
+        assert 'ssl' not in config.libraries
+    
+    @using_curl_config('curl-config-ssl-in-libs')
+    def test_ssl_in_libs(self):
+        config = pycurl_setup.ExtensionConfiguration()
+        # should link against openssl
+        assert 'ssl' in config.libraries
+    
+    @using_curl_config('curl-config-ssl-in-static-libs')
+    def test_ssl_in_static_libs(self):
+        raise nose.plugins.skip.SkipTest('this test fails')
+        
+        config = pycurl_setup.ExtensionConfiguration()
+        # should link against openssl
+        assert 'ssl' in config.libraries
