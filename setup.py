2b4d626c75001689e82e3c282ca4f9e986d671e3
Francisco Alves <chico@corp.globo.com>
2014-08-11 17:27:45 -0300

    detect ssl using curl -v if curl-config is empty
    CentOS libcurl is compiled without --static-libs
    and 'curl-config --libs' returns empty. I was able
    to detect the ssl library using 'curl -V'.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class ExtensionConfiguration(object):
         self.extra_objects = []
         self.extra_compile_args = []
         self.extra_link_args = []
-        
+
         self.configure()
 
     @property
@@ -91,6 +91,8 @@ class ExtensionConfiguration(object):
             self.include_dirs.append(os.path.join(OPENSSL_DIR, "include"))
         CURL_CONFIG = os.environ.get('PYCURL_CURL_CONFIG', "curl-config")
         CURL_CONFIG = scan_argv("--curl-config=", CURL_CONFIG)
+        CURL = os.environ.get('PYCURL_CURL', "curl")
+        CURL = scan_argv("--curl=", CURL)
         try:
             p = subprocess.Popen((CURL_CONFIG, '--version'),
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -168,13 +170,14 @@ class ExtensionConfiguration(object):
                     # first call succeeded and second call failed
                     # ignore stderr and the error exit
                     pass
+
         if optbuf == "":
             msg = "Neither curl-config --libs nor curl-config --static-libs" +\
                 " succeeded and produced output"
             if errtext:
                 msg += ":\n" + errtext
             raise ConfigurationError(msg)
-        
+
         ssl_lib_detected = False
         if 'PYCURL_SSL_LIBRARY' in os.environ:
             ssl_lib = os.environ['PYCURL_SSL_LIBRARY']
@@ -225,6 +228,34 @@ class ExtensionConfiguration(object):
                     self.define_macros.append(('HAVE_CURL_NSS', 1))
                     ssl_lib_detected = True
                     self.libraries.append('ssl3')
+
+        if not ssl_lib_detected:
+            # tries to detect using curl -V.
+            # On Centos and RedHat libcurl are built with
+            # static libraries disabled and 'curl-config --libs'
+            # returns nothing. 'curl -V' shows information
+            # about the ssl libraries.
+            p = subprocess.Popen((CURL, '-V'),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+            stdout, stderr = p.communicate()
+
+            if p.wait() == 0:
+                curl_version_info = stdout.decode()
+
+                if 'OpenSSL' in curl_version_info:
+                    self.define_macros.append(('HAVE_CURL_OPENSSL', 1))
+                    ssl_lib_detected = True
+                    self.libraries.append('crypto')
+                if not ssl_lib_detected and 'GnuTLS' in curl_version_info:
+                    self.define_macros.append(('HAVE_CURL_GNUTLS', 1))
+                    ssl_lib_detected = True
+                    self.libraries.append('gnutls')
+                if not ssl_lib_detected and 'NSS' in curl_version_info:
+                    self.define_macros.append(('HAVE_CURL_NSS', 1))
+                    ssl_lib_detected = True
+                    self.libraries.append('ssl3')
+
         if not ssl_lib_detected:
             p = subprocess.Popen((CURL_CONFIG, '--features'),
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -244,11 +275,11 @@ class ExtensionConfiguration(object):
             self.define_macros.append(('HAVE_CURL_SSL', 1))
         if not self.libraries:
             self.libraries.append("curl")
-        
+
         # Add extra compile flag for MacOS X
         if sys.platform[:-1] == "darwin":
             self.extra_link_args.append("-flat_namespace")
-        
+
         # Recognize --avoid-stdio on Unix so that it can be tested
         self.check_avoid_stdio()
 
@@ -286,9 +317,9 @@ class ExtensionConfiguration(object):
         if not os.path.exists(libcurl_lib_path):
             fail("libcurl.lib does not exist at %s.\nCurl directory must point to compiled libcurl (bin/include/lib subdirectories): %s" %(libcurl_lib_path, curl_dir))
         self.extra_objects.append(libcurl_lib_path)
-        
+
         self.check_avoid_stdio()
-        
+
         # make pycurl binary work on windows xp.
         # we use inet_ntop which was added in vista and implement a fallback.
         # our implementation will not be compiled with _WIN32_WINNT targeting
@@ -312,8 +343,8 @@ class ExtensionConfiguration(object):
         configure = configure_windows
     else:
         configure = configure_unix
-    
-    
+
+
     def check_avoid_stdio(self):
         if 'PYCURL_SETUP_OPTIONS' in os.environ and '--avoid-stdio' in os.environ['PYCURL_SETUP_OPTIONS']:
             self.extra_compile_args.append("-DPYCURL_AVOID_STDIO")
@@ -329,7 +360,7 @@ def get_bdist_msi_version_hack():
     import inspect
     import types
     import re
-    
+
     class bdist_msi_version_hack(bdist_msi):
         """ MSI builder requires version to be in the x.x.x format """
         def run(self):
@@ -350,7 +381,7 @@ def get_bdist_msi_version_hack():
             self.distribution.metadata.get_version = \
                 types.MethodType(monkey_get_version, self.distribution.metadata)
             bdist_msi.run(self)
-    
+
     return bdist_msi_version_hack
 
 
@@ -442,7 +473,7 @@ def get_data_files():
 
 def check_manifest():
     import fnmatch
-    
+
     f = open('MANIFEST.in')
     globs = []
     try:
@@ -455,7 +486,7 @@ def check_manifest():
             globs.append(glob)
     finally:
         f.close()
-    
+
     paths = []
     start = os.path.abspath(os.path.dirname(__file__))
     for root, dirs, files in os.walk(start):
@@ -466,7 +497,7 @@ def check_manifest():
                 continue
             rel = os.path.join(root, file)[len(start)+1:]
             paths.append(rel)
-    
+
     for path in paths:
         included = False
         for glob in globs:
@@ -484,11 +515,11 @@ def check_authors():
         contents = f.read()
     finally:
         f.close()
-    
+
     paras = contents.split("\n\n")
     authors_para = paras[AUTHORS_PARAGRAPH]
     authors = [author for author in authors_para.strip().split("\n")]
-    
+
     log = subprocess.check_output(['git', 'log', '--format=%an (%ae)'])
     for author in log.strip().split("\n"):
         author = author.replace('@', ' at ').replace('(', '<').replace(')', '>')
@@ -508,7 +539,7 @@ def convert_docstrings():
     for entry in sorted(os.listdir('doc/docstrings')):
         if not entry.endswith('.rst'):
             continue
-        
+
         name = entry.replace('.rst', '')
         f = open('doc/docstrings/%s' % entry)
         try:
@@ -633,7 +664,7 @@ if __name__ == "__main__":
             split_extension_source = True
         ext = get_extension(split_extension_source=split_extension_source)
         setup_args['ext_modules'] = [ext]
-        
+
         for o in ext.extra_objects:
             assert os.path.isfile(o), o
         setup(**setup_args)
