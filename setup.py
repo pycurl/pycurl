8bc5d227650191f47c8d62ae1f0149d49c58aada
Oleg Pudeyev <oleg@bsdpower.com>
2014-10-16 10:12:59 -0400

    Refactor ssl detection from linked libraries for consistency with other ssl detection paths

--- setup.py
+++ setup.py
@@ -207,24 +207,28 @@ class ExtensionConfiguration(object):
             else:
                 self.extra_link_args.append(arg)
         # ssl detection - ssl libraries are forwarded
-        for arg in split_quoted(sslhintbuf):
-            if arg[:2] == "-l":
-                if not ssl_lib_detected and arg[2:] == 'ssl':
-                    self.define_macros.append(('HAVE_CURL_OPENSSL', 1))
-                    ssl_lib_detected = True
-                    # the actual library that defines CRYPTO_num_locks etc.
-                    # is crypto, and on cygwin linking against ssl does not
-                    # link against crypto as of May 2014.
-                    # http://stackoverflow.com/questions/23687488/cant-get-pycurl-to-install-on-cygwin-missing-openssl-symbols-crypto-num-locks
-                    self.libraries.append('crypto')
-                if not ssl_lib_detected and arg[2:] == 'gnutls':
-                    self.define_macros.append(('HAVE_CURL_GNUTLS', 1))
-                    ssl_lib_detected = True
-                    self.libraries.append('gnutls')
-                if not ssl_lib_detected and arg[2:] == 'ssl3':
-                    self.define_macros.append(('HAVE_CURL_NSS', 1))
-                    ssl_lib_detected = True
-                    self.libraries.append('ssl3')
+        if not ssl_lib_detected:
+            for arg in split_quoted(sslhintbuf):
+                if arg[:2] == "-l":
+                    if arg[2:] == 'ssl':
+                        self.define_macros.append(('HAVE_CURL_OPENSSL', 1))
+                        ssl_lib_detected = True
+                        # the actual library that defines CRYPTO_num_locks etc.
+                        # is crypto, and on cygwin linking against ssl does not
+                        # link against crypto as of May 2014.
+                        # http://stackoverflow.com/questions/23687488/cant-get-pycurl-to-install-on-cygwin-missing-openssl-symbols-crypto-num-locks
+                        self.libraries.append('crypto')
+                        break
+                    if arg[2:] == 'gnutls':
+                        self.define_macros.append(('HAVE_CURL_GNUTLS', 1))
+                        ssl_lib_detected = True
+                        self.libraries.append('gnutls')
+                        break
+                    if arg[2:] == 'ssl3':
+                        self.define_macros.append(('HAVE_CURL_NSS', 1))
+                        ssl_lib_detected = True
+                        self.libraries.append('ssl3')
+                        break
         if not ssl_lib_detected:
             p = subprocess.Popen((CURL_CONFIG, '--features'),
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
