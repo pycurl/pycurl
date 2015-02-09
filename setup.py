5dc7711b710fa2a49488998fe172564dc7d3b7b8
Francisco Alves <chico@corp.globo.com>
2015-02-09 14:29:56 -0200

    changes library_dirs to self.library_dirs.
    library_dirs was undefined.

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ class ExtensionConfiguration(object):
         self.extra_objects = []
         self.extra_compile_args = []
         self.extra_link_args = []
-        
+
         self.configure()
 
     @property
@@ -84,7 +84,7 @@ class ExtensionConfiguration(object):
                 continue
             dir = os.path.normpath(dir)
             if os.path.isdir(dir):
-                if not dir in library_dirs:
+                if not dir in self.library_dirs:
                     self.library_dirs.append(dir)
             elif fatal:
                 fail("FATAL: bad directory %s in environment variable %s" % (dir, envvar))
@@ -180,7 +180,7 @@ class ExtensionConfiguration(object):
             if errtext:
                 msg += ":\n" + errtext
             raise ConfigurationError(msg)
-        
+
         ssl_lib_detected = False
         if 'PYCURL_SSL_LIBRARY' in os.environ:
             ssl_lib = os.environ['PYCURL_SSL_LIBRARY']
@@ -266,11 +266,11 @@ class ExtensionConfiguration(object):
                     self.define_macros.append(('HAVE_CURL_SSL', 1))
         if not self.libraries:
             self.libraries.append("curl")
-        
+
         # Add extra compile flag for MacOS X
         if sys.platform[:-1] == "darwin":
             self.extra_link_args.append("-flat_namespace")
-        
+
         # Recognize --avoid-stdio on Unix so that it can be tested
         self.check_avoid_stdio()
 
@@ -308,9 +308,9 @@ class ExtensionConfiguration(object):
         if not os.path.exists(libcurl_lib_path):
             fail("libcurl.lib does not exist at %s.\nCurl directory must point to compiled libcurl (bin/include/lib subdirectories): %s" %(libcurl_lib_path, curl_dir))
         self.extra_objects.append(libcurl_lib_path)
-        
+
         self.check_avoid_stdio()
-        
+
         # make pycurl binary work on windows xp.
         # we use inet_ntop which was added in vista and implement a fallback.
         # our implementation will not be compiled with _WIN32_WINNT targeting
@@ -334,8 +334,8 @@ class ExtensionConfiguration(object):
         configure = configure_windows
     else:
         configure = configure_unix
-    
-    
+
+
     def check_avoid_stdio(self):
         if 'PYCURL_SETUP_OPTIONS' in os.environ and '--avoid-stdio' in os.environ['PYCURL_SETUP_OPTIONS']:
             self.extra_compile_args.append("-DPYCURL_AVOID_STDIO")
@@ -399,7 +399,7 @@ def get_bdist_msi_version_hack():
     import inspect
     import types
     import re
-    
+
     class bdist_msi_version_hack(bdist_msi):
         """ MSI builder requires version to be in the x.x.x format """
         def run(self):
@@ -420,7 +420,7 @@ def get_bdist_msi_version_hack():
             self.distribution.metadata.get_version = \
                 types.MethodType(monkey_get_version, self.distribution.metadata)
             bdist_msi.run(self)
-    
+
     return bdist_msi_version_hack
 
 
@@ -512,7 +512,7 @@ def get_data_files():
 
 def check_manifest():
     import fnmatch
-    
+
     f = open('MANIFEST.in')
     globs = []
     try:
@@ -525,7 +525,7 @@ def check_manifest():
             globs.append(glob)
     finally:
         f.close()
-    
+
     paths = []
     start = os.path.abspath(os.path.dirname(__file__))
     for root, dirs, files in os.walk(start):
@@ -536,7 +536,7 @@ def check_manifest():
                 continue
             rel = os.path.join(root, file)[len(start)+1:]
             paths.append(rel)
-    
+
     for path in paths:
         included = False
         for glob in globs:
@@ -554,11 +554,11 @@ def check_authors():
         contents = f.read()
     finally:
         f.close()
-    
+
     paras = contents.split("\n\n")
     authors_para = paras[AUTHORS_PARAGRAPH]
     authors = [author for author in authors_para.strip().split("\n")]
-    
+
     log = subprocess.check_output(['git', 'log', '--format=%an (%ae)'])
     for author in log.strip().split("\n"):
         author = author.replace('@', ' at ').replace('(', '<').replace(')', '>')
@@ -578,7 +578,7 @@ def convert_docstrings():
     for entry in sorted(os.listdir('doc/docstrings')):
         if not entry.endswith('.rst'):
             continue
-        
+
         name = entry.replace('.rst', '')
         f = open('doc/docstrings/%s' % entry)
         try:
@@ -704,7 +704,7 @@ if __name__ == "__main__":
             split_extension_source = True
         ext = get_extension(sys.argv, split_extension_source=split_extension_source)
         setup_args['ext_modules'] = [ext]
-        
+
         for o in ext.extra_objects:
             assert os.path.isfile(o), o
         setup(**setup_args)
