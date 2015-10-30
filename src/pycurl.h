5d82769f81091cb4ae41229c4323f5d5d9aa83b0
Oleg Pudeyev <oleg@bsdpower.com>
2015-10-30 00:14:52 -0400

    Wrap in WIN32 for good measure

--- src/pycurl.h
+++ src/pycurl.h
@@ -16,14 +16,16 @@
 #include <arpa/inet.h>
 #endif
 
+#if defined(WIN32)
 /*
  * Since setup.py uses a '-WX' in the CFLAGS (treat warnings as errors),
  * the below will turn off some warnings when using MS-SDK 8.1+.
  * This MUST be defined before including <winsock2.h> via the libcurl
  * headers.
  */
-#ifndef _WINSOCK_DEPRECATED_NO_WARNINGS
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+# if !defined(_WINSOCK_DEPRECATED_NO_WARNINGS)
+#  define _WINSOCK_DEPRECATED_NO_WARNINGS
+# endif
 #endif
 
 #include <curl/curl.h>
