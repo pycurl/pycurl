e8805cbd482d77cc020cb2ada1627c8f612193be
Oleg Pudeyev <oleg@bsdpower.com>
2015-12-13 14:41:55 -0500

    Rename the file to match others

--- tests/socket_open_cb_test.py
+++ tests/socket_open_cb_test.py
@@ -19,26 +19,26 @@ def socket_open(family, socktype, protocol, address):
     global socket_open_address
     socket_open_called = True
     socket_open_address = address
-    
+
     #print(family, socktype, protocol, address)
     s = socket.socket(family, socktype, protocol)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
     return s
 
-class SocketOpenTest(unittest.TestCase):
+class SocketOpenCbTest(unittest.TestCase):
     def setUp(self):
         self.curl = pycurl.Curl()
-    
+
     def tearDown(self):
         self.curl.close()
-    
+
     def test_socket_open(self):
         self.curl.setopt(pycurl.OPENSOCKETFUNCTION, socket_open)
         self.curl.setopt(self.curl.URL, 'http://localhost:8380/success')
         sio = util.BytesIO()
         self.curl.setopt(pycurl.WRITEFUNCTION, sio.write)
         self.curl.perform()
-        
+
         assert socket_open_called
         self.assertEqual(("127.0.0.1", 8380), socket_open_address)
         self.assertEqual('success', sio.getvalue().decode())
