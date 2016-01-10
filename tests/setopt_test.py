c8b07171b02ab5f28b323ff10474c0768aa60ec9
Oleg Pudeyev <oleg@bsdpower.com>
2016-01-10 15:02:30 -0500

    Whitespace

--- tests/setopt_test.py
+++ tests/setopt_test.py
@@ -9,30 +9,30 @@ import nose.tools
 class SetoptTest(unittest.TestCase):
     def setUp(self):
         self.curl = pycurl.Curl()
-    
+
     def tearDown(self):
         self.curl.close()
-    
+
     def test_boolean_value(self):
         # expect no exceptions raised
         self.curl.setopt(pycurl.VERBOSE, True)
-    
+
     def test_integer_value(self):
         # expect no exceptions raised
         self.curl.setopt(pycurl.VERBOSE, 1)
-    
+
     @nose.tools.raises(TypeError)
     def test_string_value_for_integer_option(self):
         self.curl.setopt(pycurl.VERBOSE, "Hello, world!")
-    
+
     def test_string_value(self):
         # expect no exceptions raised
         self.curl.setopt(pycurl.URL, 'http://hello.world')
-    
+
     @nose.tools.raises(TypeError)
     def test_integer_value_for_string_option(self):
         self.curl.setopt(pycurl.URL, 1)
-    
+
     @nose.tools.raises(TypeError)
     def test_float_value_for_integer_option(self):
         self.curl.setopt(pycurl.VERBOSE, 1.0)
