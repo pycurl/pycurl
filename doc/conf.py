ef36675e931f19f92638f5cf62b56958baa322f5
Oleg Pudeyev <oleg@bsdpower.com>
2017-01-31 10:54:59 -0500

    Update copyrights to 2017

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PycURL'
-copyright = u'2001-2016 Kjetil Jacobsen, Markus F.X.J. Oberhumer, Oleg Pudeyev'
+copyright = u'2001-2017 Kjetil Jacobsen, Markus F.X.J. Oberhumer, Oleg Pudeyev'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
