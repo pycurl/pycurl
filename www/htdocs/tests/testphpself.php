bbebfe3054b4507c826bfa057f9ae0ad827c2c1b
Oleg Pudeyev <oleg@bsdpower.com>
2018-06-02 00:30:58 -0400

    Remove www dir, this has not been used in a couple of years

--- www/htdocs/tests/testphpself.php
+++
@@ -1,7 +0,0 @@
-<?php // vi:ts=4:et
-header("Content-type: text/plain");
-
-echo "request: ${_SERVER['PHP_SELF']}";
-if ($_SERVER['QUERY_STRING'])
-    echo "?${_SERVER['QUERY_STRING']}";
-echo "\n";
