b2e4262ff3af28c1239758885c474e4467f2b309
Scott Talbert <swt@techie.net>
2024-08-11 17:09:12 -0400

    Declare support for Python 3.13

--- setup.py
+++ setup.py
@@ -852,7 +852,7 @@ libcurl, including:
 Requirements
 ------------
 
-- Python 3.5-3.12.
+- Python 3.5-3.13.
 - libcurl 7.19.0 or better.
 
 
@@ -929,6 +929,7 @@ in COPYING-LGPL_ and COPYING-MIT_ files in the source distribution.
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Internet :: File Transfer Protocol (FTP)',
         'Topic :: Internet :: WWW/HTTP',
     ],
