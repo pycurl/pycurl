60c7aeae14be44ff35e83f4239f27238389d85a5
Jorge Rocamora <33847633+aeroyorch@users.noreply.github.com>
2026-01-25 11:10:49 +0000

    Integrate CURL_MIMEPOST option and unify implementation to use MIME instead of HTTPPOST for libcurl versions greater or equal to 7.56.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,9 @@ import pytest
 import time
 from typing import Generator
 
-from . import appmanager, localhost
+import pycurl
+
+from . import appmanager, localhost, util
 
 
 def _get_free_port() -> int:
@@ -25,6 +27,19 @@ def wait_listening(host: str, port: int, timeout: float = 5.0) -> None:
     raise RuntimeError(f"App did not start listening on {host}:{port}")
 
 
+POST_OPTS = [pycurl.HTTPPOST]
+POST_OPT_IDS = {
+    pycurl.HTTPPOST: "httppost",
+}
+if not util.pycurl_version_less_than(7, 56, 0):
+    POST_OPTS.append(pycurl.MIMEPOST)
+    POST_OPT_IDS[pycurl.MIMEPOST] = "mimepost"
+
+
+def post_opt_id(opt):
+    return POST_OPT_IDS.get(opt, str(opt))
+
+
 @pytest.fixture
 def free_port() -> int:
     return _get_free_port()
@@ -43,3 +58,8 @@ def app(request) -> Generator[str, None, None]:
     wait_listening(localhost, port, timeout=10.0)
     yield f"http://{localhost}:{port}"
     teardown(request.module)
+
+
+@pytest.fixture(params=POST_OPTS, ids=post_opt_id)
+def post_opt(request):
+    return request.param
