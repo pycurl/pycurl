3c2d9f21076a001c857ccf85007d77fbfc7de4bd
Jorge Rocamora <33847633+aeroyorch@users.noreply.github.com>
2026-02-03 17:27:06 +0100

    Fix pause thread state and expand/simplify pause tests

--- src/easyperform.c
+++ src/easyperform.c
@@ -88,6 +88,7 @@ do_curl_pause(CurlObject *self, PyObject *args)
     CURLcode res;
 #ifdef WITH_THREAD
     PyThreadState *saved_state;
+    PyThreadState *saved_multi_state;
 #endif
 
     if (!PyArg_ParseTuple(args, "i:pause", &bitmask)) {
@@ -100,26 +101,26 @@ do_curl_pause(CurlObject *self, PyObject *args)
 #ifdef WITH_THREAD
     /* Save handle to current thread (used as context for python callbacks) */
     saved_state = self->state;
-    PYCURL_BEGIN_ALLOW_THREADS_EASY
-
+    saved_multi_state = self->multi_stack->state;
+    
     /* We must allow threads here because unpausing a handle can cause
        some of its callbacks to be invoked immediately, from inside
        curl_easy_pause() */
 #endif
-
+    
+    PYCURL_BEGIN_ALLOW_THREADS_EASY
     res = curl_easy_pause(self->handle, bitmask);
-
-#ifdef WITH_THREAD
     PYCURL_END_ALLOW_THREADS_EASY
 
+#ifdef WITH_THREAD
     /* Restore the thread-state to whatever it was on entry */
     self->state = saved_state;
+    self->multi_stack->state = saved_multi_state;
 #endif
 
     if (res != CURLE_OK) {
         CURLERROR_MSG("pause/unpause failed");
     } else {
-        Py_INCREF(Py_None);
-        return Py_None;
+        Py_RETURN_NONE;
     }
 }
