042f8a49e4306fba5d568eae03d602ce20ce9161
Jorge Rocamora <33847633+aeroyorch@users.noreply.github.com>
2026-02-14 00:26:18 +0100

    remove commented prints

--- tests/multi_socket_select_test.py
+++ tests/multi_socket_select_test.py
@@ -58,11 +58,9 @@ class MultiSocketSelectTest(unittest.TestCase):
         # socket callback
         def socket(event, socket, multi, data):
             if event == pycurl.POLL_REMOVE:
-                # print("Remove Socket %d"%socket)
                 sockets.remove(socket)
             else:
                 if socket not in sockets:
-                    # print("Add socket %d"%socket)
                     sockets.add(socket)
             socket_events.append((event, multi))
 
