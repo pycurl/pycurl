9a3ef72b4df6ede44bb98c8a0f22345658be408c
Scott Talbert <swt@techie.net>
2026-05-09 21:16:30 -0400

    Modernize ssh_key_cb_test and use a local SFTP server
    This avoids requiring internet access to run the tests.

--- tests/ssh_key_cb_test.py
+++ tests/ssh_key_cb_test.py
@@ -1,89 +1,51 @@
-#! /usr/bin/env python
-# vi:ts=4:et
-
-import unittest
-import pycurl
 import pytest
+import pycurl
 
 from . import util
 
-sftp_server = 'sftp://web.sourceforge.net'
-
-@pytest.mark.online
-@pytest.mark.ssh
-class SshKeyCbTest(unittest.TestCase):
-    '''This test requires Internet access.'''
-
-    def setUp(self):
-        self.curl = util.DefaultCurl()
-        self.curl.setopt(pycurl.URL, sftp_server)
-        self.curl.setopt(pycurl.VERBOSE, True)
-
-    def tearDown(self):
-        self.curl.close()
-
-    @util.min_libcurl(7, 19, 6)
-    # curl compiled with libssh doesn't support
-    # CURLOPT_SSH_KNOWNHOSTS and CURLOPT_SSH_KEYFUNCTION
-    @util.guard_unknown_libcurl_option
-    def test_keyfunction(self):
-        # with keyfunction returning ok
-
-        def keyfunction(known_key, found_key, match):
-            return pycurl.KHSTAT_FINE
-
-        self.curl.setopt(pycurl.SSH_KNOWNHOSTS, '.known_hosts')
-        self.curl.setopt(pycurl.SSH_KEYFUNCTION, keyfunction)
-
-        try:
-            self.curl.perform()
-            self.fail('should have raised')
-        except pycurl.error as e:
-            self.assertEqual(pycurl.E_LOGIN_DENIED, e.args[0])
-
-        # with keyfunction returning not ok
-
-        def keyfunction(known_key, found_key, match):
-            return pycurl.KHSTAT_REJECT
-
-        self.curl.setopt(pycurl.SSH_KNOWNHOSTS, '.known_hosts')
-        self.curl.setopt(pycurl.SSH_KEYFUNCTION, keyfunction)
-
-        try:
-            self.curl.perform()
-            self.fail('should have raised')
-        except pycurl.error as e:
-            self.assertEqual(pycurl.E_PEER_FAILED_VERIFICATION, e.args[0])
-
-    @util.min_libcurl(7, 19, 6)
-    @util.guard_unknown_libcurl_option
-    def test_keyfunction_bogus_return(self):
-        def keyfunction(known_key, found_key, match):
-            return 'bogus'
-
-        self.curl.setopt(pycurl.SSH_KNOWNHOSTS, '.known_hosts')
-        self.curl.setopt(pycurl.SSH_KEYFUNCTION, keyfunction)
-
-        try:
-            self.curl.perform()
-            self.fail('should have raised')
-        except pycurl.error as e:
-            self.assertEqual(pycurl.E_PEER_FAILED_VERIFICATION, e.args[0])
+pytestmark = pytest.mark.ssh
 
 
-@pytest.mark.ssh
-class SshKeyCbUnsetTest(unittest.TestCase):
-    def setUp(self):
-        self.curl = util.DefaultCurl()
-        self.curl.setopt(pycurl.URL, sftp_server)
-        self.curl.setopt(pycurl.VERBOSE, True)
+@util.min_libcurl(7, 19, 6)
+@util.guard_unknown_libcurl_option
+def test_keyfunction_fine(sftp_curl, known_hosts_file):
+    sftp_curl.setopt(pycurl.SSH_KNOWNHOSTS, known_hosts_file)
+    sftp_curl.setopt(pycurl.SSH_KEYFUNCTION, lambda known_key, found_key, match: pycurl.KHSTAT_FINE)
 
-    @util.min_libcurl(7, 19, 6)
-    @util.guard_unknown_libcurl_option
-    def test_keyfunction_none(self):
-        self.curl.setopt(pycurl.SSH_KEYFUNCTION, None)
+    with pytest.raises(pycurl.error) as exc_info:
+        sftp_curl.perform()
+    assert exc_info.value.args[0] == pycurl.E_LOGIN_DENIED
 
-    @util.min_libcurl(7, 19, 6)
-    @util.guard_unknown_libcurl_option
-    def test_keyfunction_unset(self):
-        self.curl.unsetopt(pycurl.SSH_KEYFUNCTION)
+
+@util.min_libcurl(7, 19, 6)
+@util.guard_unknown_libcurl_option
+def test_keyfunction_reject(sftp_curl, known_hosts_file):
+    sftp_curl.setopt(pycurl.SSH_KNOWNHOSTS, known_hosts_file)
+    sftp_curl.setopt(pycurl.SSH_KEYFUNCTION, lambda known_key, found_key, match: pycurl.KHSTAT_REJECT)
+
+    with pytest.raises(pycurl.error) as exc_info:
+        sftp_curl.perform()
+    assert exc_info.value.args[0] == pycurl.E_PEER_FAILED_VERIFICATION
+
+
+@util.min_libcurl(7, 19, 6)
+@util.guard_unknown_libcurl_option
+def test_keyfunction_bogus_return(sftp_curl, known_hosts_file):
+    sftp_curl.setopt(pycurl.SSH_KNOWNHOSTS, known_hosts_file)
+    sftp_curl.setopt(pycurl.SSH_KEYFUNCTION, lambda known_key, found_key, match: 'bogus')
+
+    with pytest.raises(pycurl.error) as exc_info:
+        sftp_curl.perform()
+    assert exc_info.value.args[0] == pycurl.E_PEER_FAILED_VERIFICATION
+
+
+@util.min_libcurl(7, 19, 6)
+@util.guard_unknown_libcurl_option
+def test_keyfunction_set_none(sftp_curl):
+    sftp_curl.setopt(pycurl.SSH_KEYFUNCTION, None)
+
+
+@util.min_libcurl(7, 19, 6)
+@util.guard_unknown_libcurl_option
+def test_keyfunction_unset(sftp_curl):
+    sftp_curl.unsetopt(pycurl.SSH_KEYFUNCTION)
